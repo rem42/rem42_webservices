fac25ed075ad23506be1c298a2eef203560d15ff
rem42 <remy492@hotmail.fr>
2019-05-09 19:23:39 +0200

    new test 2

--- rem42_webservices.php
+++ rem42_webservices.php
@@ -1,65 +1,73 @@
 <?php
 
 if (!defined('_PS_VERSION_')) {
-	exit;
+    exit;
 }
 
 require_once 'classes/WebserviceSpecificManagementRem42Webservices.php';
 
 class Rem42_webservices extends Module
 {
-	protected $config_form = false;
+    protected $config_form = false;
 
-	public function __construct()
-	{
-		$this->name          = 'rem42_webservices';
-		$this->tab           = 'others';
-		$this->version       = '1.0.0';
-		$this->author        = 'rem42';
-		$this->need_instance = 0;
+    public function __construct()
+    {
+        $this->name          = 'rem42_webservices';
+        $this->tab           = 'others';
+        $this->version       = '1.0.0';
+        $this->author        = 'rem42';
+        $this->need_instance = 0;
 
-		/**
-		 * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
-		 */
-		$this->bootstrap = true;
+        /**
+         * Set $this->bootstrap to true if your module is compliant with bootstrap (PrestaShop 1.6)
+         */
+        $this->bootstrap = true;
 
-		parent::__construct();
+        parent::__construct();
 
-		$this->displayName = $this->l('Extends webservices');
-		$this->description = $this->l('Add new missing webservices for prestashop 1.6 && 1.7');
+        $this->displayName = $this->l('Extends webservices');
+        $this->description = $this->l('Add new missing webservices for prestashop 1.6 && 1.7');
 
-		$this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
-	}
+        $this->ps_versions_compliancy = ['min' => '1.6', 'max' => _PS_VERSION_];
+    }
 
-	/**
-	 * Don't forget to create update methods if needed:
-	 * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
-	 */
+    /**
+     * Don't forget to create update methods if needed:
+     * http://doc.prestashop.com/display/PS16/Enabling+the+Auto-Update
+     */
     public function install()
     {
         return parent::install() &&
             $this->registerHook('addWebserviceResources');
     }
 
-	public function uninstall()
-	{
-		return parent::uninstall();
-	}
+    public function uninstall()
+    {
+        return parent::uninstall();
+    }
 
-	/**
-	 * Load the configuration form
-	 */
-	public function getContent()
-	{
-		$this->context->smarty->assign('module_dir', $this->_path);
+    /**
+     * Load the configuration form
+     */
+    public function getContent()
+    {
+        $this->context->smarty->assign('module_dir', $this->_path);
 
-		$output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
+        $output = $this->context->smarty->fetch($this->local_path . 'views/templates/admin/configure.tpl');
 
-		return $output;
-	}
+        return $output;
+    }
 
-	public function hookAddWebserviceResources(array $resources)
-	{
-		return ['description' => 'Extends Webservices', 'specific_management' => true];
-	}
+    public function hookAddWebserviceResources(array $resources)
+    {
+        return self::getWebserviceResources();
+    }
+
+    public static function getWebserviceResources()
+    {
+        $resources                      = ['description' => 'Extends Webservices', 'specific_management' => true];
+        $resources['rem42_webservices'] = ['description' => 'Extends Webservices', 'specific_management' => true];
+
+        return $resources;
+    }
 }
