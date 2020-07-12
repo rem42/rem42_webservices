71868a35cee94db5128d0e12a1b18c1be67a29d2
rem42 <me@remy.ovh>
2020-07-12 16:41:23 +0200

    update with older version of prestashop 1.7

--- override/classes/webservice/WebserviceRequest.php
+++ override/classes/webservice/WebserviceRequest.php
@@ -7,4 +7,20 @@ class WebserviceRequest extends WebserviceRequestCore
     {
         return $this->_inputXml;
     }
+
+    public static function getResources()
+    {
+        $resources = WebserviceRequestCore::getResources();
+
+        if (version_compare(_PS_VERSION_, '1.7', '>=')) {
+            return $resources;
+        }
+
+        $hookResources = Hook::exec('addWebserviceResources', array('resources' => $resources), null, true, false);
+
+        $resources = array_merge($resources, $hookResources);
+
+        ksort($resources);
+        return $resources;
+    }
 }
